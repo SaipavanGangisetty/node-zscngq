e1e13ed281808740c85967b8ad0b84d8ef224cb7
SaipavanGangisetty <83353157+SaipavanGangisetty@users.noreply.github.com>
2023-02-14 22:00:03 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# node-zscngq
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/node-zscngq)
---
+++ index.js
@@ -0,0 +1,16 @@
+// run `node index.js` in the terminal
+process.stdin.resume();
+process.stdin.setEncoding('ascii');
+process.stdin.on('data', function (input) {
+  for (i = 1; i <= 100; i++) {
+    if (i % 3 === 0 && i % 5 === 0) {
+      console.log('amazon');
+    } else if (i % 3 === 0) {
+      console.log('google');
+    } else if (i % 5 === 0) {
+      console.log('facebook');
+    } else {
+      console.log(i);
+    }
+  }
+});
